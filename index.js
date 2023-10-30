6e6a0269acd327df42dcc316ec3ddf02b9539c78
JahirPendhari <jahirpp1999@gmail.com>
2023-10-30 16:07:23 +0530

    backend

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { connection } = require("./db");
+const { userRoute } = require("./routes/user.routes");
+const { postRoute } = require("./routes/post.routes");
+
+const app = express();
+
+app.use(express());
+app.use(express.json())
+app.use("/users",userRoute)
+app.use("/posts",postRoute)
+
+
+
+app.listen(process.env.port, async()=>{
+    try{
+        await connection
+        console.log("Mongodb atlas is connected ")
+        console.log("Server is Conntected")
+    }catch(err){
+        console.log(err)
+    }
+})
